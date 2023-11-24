67d7377a3131b3e84406ae87b197ed7ff91b5876
alejandro montero <alejandromonteroart1551@gmail.com>
2023-11-23 20:15:37 -0400

    update

--- src/config/session.config.ts
+++ src/config/session.config.ts
@@ -6,13 +6,12 @@ import RedisStore from 'connect-redis';
 
 const client = createClient({
   username: 'default',
-  password: process.env['REDIS_PASSWORD'],
+  password: 'Yl2VWc9ZpfaNSaXpsG1tZmjuXTzok8ls',
   socket: {
-    host: process.env['REDIS_HOST'],
-    port: parseInt(process.env['REDIS_PORT'],
+    host: 'redis-17401.c322.us-east-1-2.ec2.cloud.redislabs.com',
+    port: 17401,
   },
 });
-
 client.on('error', (err) => console.log('Redis Client Error', err));
 
 async function setupRedis() {
@@ -38,7 +37,7 @@ export const redisSession = session({
     maxAge: 24 * 60 * 60 * 1000,
   },
   store: store,
-  genid: (req) => {
+  genid: () => {
     return uuidv4();
   },
 });
